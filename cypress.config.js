0f6406e6fe78eaa06337b89bc2c8ac85e468c175
mohammadpy8 <mohammadpy8@gmail.com>
2023-10-07 14:44:17 +0330

    loooo

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,41 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "5afu2u",
+  experimentalSessionAndOrigin: true,
   e2e: {
+    // baseUrl: 'https://stage1.qhami.com/',
+    experimentalStudio: true,
+    video: true,
+    videoCompression: 32,
+    viewportHeight: 900,
+    viewportWidth: 550,
+    videoUploadOnPasses: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        // deconstruct the individual properties
+        hello({ greeting, name }) {
+          console.log("%s, %s", greeting, name);
+
+          return null;
+        },
+        loginTask(obj) {
+          console.log("------------------", obj);
+          return null;
+        },
+        runLoadTests(userCount) {
+          const users = [];
+
+          for (let i = 1; i <= userCount; i++) {
+            users.push({
+              username: `user${i}`,
+              password: `pass${i}`,
+            });
+          }
+
+          return users;
+        },
+      });
     },
   },
 });
--- cypress/e2e/mresalat/Login.cy.js
+++ cypress/e2e/mresalat/Login.cy.js
@@ -1,80 +1,127 @@
 /// <reference types="Cypress" />
 
 describe("login", () => {
+  const BASE_URL = "https://stage1.qhami.com";
+  const MHESAM_URL = "https://stage1.fardaap.com";
 
-    const BASE_URL = "https://stage1.qhami.com/";
-    const MHESAM_URL = "https://stage1.fardaap.com"
+  it("open web", () => {
+    cy.visit(BASE_URL);
 
-    it("open web", () => {
-        cy.visit(BASE_URL);
+    cy.wait(500);
 
-        cy.wait(500)
+    cy.get("#profile > [href='/']").click();
 
-        cy.get("#profile > [href='/']").click();
+    cy.wait(500);
+
+    cy.url().should("include", "sso/login");
+
+    // cy.wait(500);
+
+    cy.wait(500);
+
+    cy.get("input[name='username']").click().type("09550000012");
+
+    cy.wait(500);
+
+    cy.get("input[name='password']").click().type("123456789");
+
+    cy.wait(500);
+
+    cy.get("#captchaValue").click().type("111111");
+
+    cy.get(".flex-sb-m > div > .txt1").click();
+
+    cy.get("#username").click().type("00000");
+
+    cy.get("#nationalCode").click().type("00000");
+    cy.get("#captchaValue").click().type("0000000");
+    cy.get(".login100-form-btn").click();
+    cy.wait(500);
+    cy.get(".swal2-confirm").click();
+    cy.get(".back_btn").click();
+
+    cy.get("input[name='username']").click().type("09550000012");
+
+    cy.wait(500);
+
+    cy.get("input[name='password']").click().type("123456789");
+
+    cy.wait(500);
+
+    cy.get("#captchaValue").click().type("111111");
+    cy.get(".login100-form-btn").click();
+
+    cy.url().should("include", "qhami.com");
+
+    cy.wait(500);
+
+    cy.get(
+      '.cardtwo > :nth-child(1) > [style="display: flex; text-align: center;"] > [style="display: flex; width: 100%; padding: 2% 1%; border-radius: 10px; align-items: center; justify-content: center; margin: auto; flex-direction: column;"] > :nth-child(1)'
+    ).click({ force: true });
+
+    cy.contains("ورود به ام حسام").click({ force: true });
+
+    cy.origin(MHESAM_URL, () => {
+      const amount = 1;
+      const mqr = 9;
+      let haseCreaditPlus = false;
+      let token;
+
+      cy.then(() => {
+        cy.getAllLocalStorage().then((result) => {
+          console.log(result);
+          let json = JSON.parse(result["https://stage1.qhami.com"].mresalatPwa);
+          console.log(json);
+          token = json.token.access_token;
+          console.log(token);
+        });
+      }).then(() => {
+        cy.wait(500);
+
+        cy.get("#profile > div").click({ force: true });
 
         cy.wait(500);
 
-        cy.url().should("include", "/login");
-
-        // cy.wait(500);
+        cy.get("input[type='text']").click().type(mqr);
 
         cy.wait(500);
 
-        cy.get("input[name='username']").click().type("09550000012")
+        cy.get(":nth-child(2) > :nth-child(3) > .sc-bczRLJ").click();
 
         cy.wait(500);
 
-        cy.get("input[name='password']").click().type("123456789")
-
-        cy.wait(500);
-
-        cy.get("#captchaValue").click().type("111111")
-
-        cy.get(".flex-sb-m > div > .txt1").click();
-
-        cy.get('#username').click().type("00000");
-
-        cy.get('#nationalCode').click().type("00000");
-        cy.get('#captchaValue').click().type("0000000");
-        cy.get('.login100-form-btn').click()
-        cy.wait(500);
-        cy.get('.swal2-confirm').click();
-        cy.get('.back_btn').click();
-
-        cy.get("input[name='username']").click().type("09550000012")
-
-        cy.wait(500);
-
-        cy.get("input[name='password']").click().type("123456789")
-
-        cy.wait(500);
-
-        cy.get("#captchaValue").click().type("111111")
-        cy.get('.login100-form-btn').click();
-
-        cy.url().should("include", "qhami.com");
-
-        cy.wait(500);
-
-        cy.get('.cardtwo > :nth-child(1) > [style="display: flex; text-align: center;"] > [style="display: flex; width: 100%; padding: 2% 1%; border-radius: 10px; align-items: center; justify-content: center; margin: auto; flex-direction: column;"] > :nth-child(1)').click({force: true})
-
-        cy.contains("ورود به ام حسام").click({force: true});
-
-        cy.origin(MHESAM_URL, () => {
-            const amount = 1;
-            const mqr = 9;
-            let haseCreaditPlus = false;
-            let token;
+        cy.request({
+          method: "GET",
+          url: `https://stage1api.qhami.com/mhesam/mqr/get-sat-info/${mqr}`,
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((response) => {
+          cy.request({
+            method: "GET",
+            url: `https://stage1api.qhami.com/mhesam/mqr/mqr-credit?checkCreditRequestModel={"promotedMemberUserId":${response.body.terminalFacilityId},"parcelAmount":${amount}}`,
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((response) => {
+            let amountFinal = 0;
 
             cy.then(() => {
-                cy.getAllLocalStorage().then((result) => {
-                    let json = JSON.parse(result["https://stage1.qhami.com"].mresalatPwa);
-                    token = json.token.access_token
-                    console.log(token);
-                })
-            })
-        })
-
-
-    })
-})
+              console.log(response.body);
+              response.body.creditFacilityHesanResponseModels.map((items) => {
+                if (items.facilityModel) {
+                  items.creditFacilityHesanListModelList.map((finall) => {
+                    amountFinal += finall.availableAmount;
+                    console.log(amountFinal);
+                  });
+                }
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
